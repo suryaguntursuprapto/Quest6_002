c1facd1962dceddd8c638fab6788050047835f35
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-11-21 20:24:07 +0700

    membuat object MataKuliah

--- app/src/main/java/com/example/latihannavigasi/data/MataKuliah.kt
+++ app/src/main/java/com/example/latihannavigasi/data/MataKuliah.kt
@@ -1,1 +1,12 @@
 package com.example.latihannavigasi.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemprograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
