f0576e988d7dfbee4a3d43dba021e18e33b6c35e
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-11-22 12:17:38 +0700

    Tambahkan fungsi untuk aplikasi Mahasiswa serta mensetting widgetnya

--- app/src/main/java/com/example/latihannavigasi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/latihannavigasi/navigation/PengelolaHalaman.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.latihannavigasi.ui.view.viewmodel.MahasiswaViewModel
 import com.example.latihannavigasi.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -14,3 +18,12 @@ enum class Halaman{
     Tampil
 }
 
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+
+}
