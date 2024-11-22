e7c414642f1503f147287d3dbfd0732f27372457
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-11-22 12:07:44 +0700

    Tambahkan enum untuk mendefinisikan navigasi antar halaman

--- app/src/main/java/com/example/latihannavigasi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/latihannavigasi/navigation/PengelolaHalaman.kt
@@ -1,2 +1,16 @@
 package com.example.latihannavigasi.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import com.example.latihannavigasi.ui.view.viewmodel.MahasiswaViewModel
+import com.example.latihannavigasi.ui.view.viewmodel.RencanaStudyViewModel
+
+enum class Halaman{
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
